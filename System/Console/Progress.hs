3f3c581be341d1683c7950ffe236dc1b26085cda
marius a. eriksen <marius@monkey.org>
2009-09-02 22:58:26 -0700

    Some refactoring, ProgressM

--- System/Console/Progress.hs
+++ System/Console/Progress.hs
@@ -4,27 +4,32 @@ module System.Console.Progress
   ( Progress
   , empty
   , update
-  , project
+  , projectTimeLeft
   , pctDone
-  , draw
+  , drawProgressBar
+  -- * The ProgressM monad.
+  , ProgressM
+  , runProgress
+  , resetProgress
+  , displayProgressBar
   ) where
 
 import Data.Time.Clock (diffUTCTime, UTCTime, getCurrentTime)
 import Text.Printf (printf, PrintfArg)
 import Control.Monad.State (StateT, runStateT, MonadState
-                           , MonadIO, modify, get, liftIO)
+                           , MonadIO, modify, get, put, liftIO)
 
-data Progress a = Progress a a UTCTime
+data Progress a = Progress a a UTCTime Int
 
 empty :: (Num a) => a -> UTCTime -> Progress a
-empty total now = Progress 0 total now
+empty total now = Progress 0 total now 0
 
 update :: (Num a) => a -> Progress a -> Progress a
-update howmany (Progress count total time) = 
-  Progress (count + howmany) total time
+update howmany (Progress count total time width) = 
+  Progress (count + howmany) total time width
 
-project :: (Integral a) => UTCTime -> Progress a -> Maybe a
-project when (Progress count total time) =
+projectTimeLeft :: (Integral a) => UTCTime -> Progress a -> Maybe a
+projectTimeLeft when (Progress count total time _) =
   if rate > 0
      then Just $ floor $ (toRational left) / rate
      else Nothing
@@ -34,17 +39,26 @@ project when (Progress count total time) =
     diffSecs = toRational $ diffUTCTime when time
 
 pctDone :: (Integral a) => Progress a -> a
-pctDone (Progress count total _) = (100 * count) `div` total
+pctDone (Progress count total _ _) = (100 * count) `div` total
 
-draw :: (PrintfArg a, Integral a) => UTCTime -> Progress a -> String
-draw when p@(Progress count total time) =
-  printf "%s done, remaining: %s" pct left :: String
-  where 
+drawProgressBar :: (PrintfArg a, Integral a) => 
+                   UTCTime 
+                -> Progress a 
+                -> (Progress a, String)
+drawProgressBar when p@(Progress count total time width) =
+  (Progress count total time (max width (length str)), str)
+  where
+    width' = (max width (length str))
+
+    str = (printf "%s done, remaining: %s" pct left) ++ padding
+
+    padding = replicate (width' - (length str)) ' '
+
     pct :: String
     pct = printf "%02d%%" $ pctDone p
 
     left :: String
-    left = leftStr $ project when p
+    left = leftStr $ projectTimeLeft when p
     leftStr Nothing = "?"
     leftStr (Just s)
       | s < 60    = printf "%02ds" s
@@ -54,18 +68,21 @@ draw when p@(Progress count total time) =
 newtype ProgressM a = ProgressM (StateT (Progress Integer) IO a)
                       deriving (Monad, MonadIO, MonadState (Progress Integer))
 
-reset :: Integer -> ProgressM ()
-reset howmany = do
+resetProgress :: Integer -> ProgressM ()
+resetProgress howmany = do
   t <- liftIO $ getCurrentTime
   modify $ \p -> empty howmany t
 
 makeProgress :: Integer -> ProgressM ()
 makeProgress howmuch = modify $ update howmuch
 
-displayProgress :: ProgressM ()
-displayProgress = do
+displayProgressBar :: ProgressM ()
+displayProgressBar = do
   t <- liftIO $ getCurrentTime
-  get >>= liftIO . putStr . draw t
+  d <- get
+  let (d', str) = drawProgressBar t d
+  put d'
+  liftIO $ putStr str
 
 runProgress :: Integer -> ProgressM a -> IO a
 runProgress howmany (ProgressM action) =
